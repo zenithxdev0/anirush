f857cd56f5ae087056eacbc84741a2c40d18e445
KarlAngeloFlores <floreskarlangelo03@gmail.com>
2025-05-29 11:51:41 +0800

    fixed path for search

--- src/features/searchSlice.js
+++ src/features/searchSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import searchService from "../services/SearchService";
+import searchService from "../services/searchService";
 
 
 export const fetchSearchSuggestion = createAsyncThunk(
